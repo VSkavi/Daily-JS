7c9de11fc8e7ed222f4f730477d0f3a9add20007
Kavi VS <vskavi20@gmail.com>
2026-05-24 11:18:32 +0530

    Finf largest number

---
+++ largest-number.js
@@ -0,0 +1,12 @@
+let num1 = 26
+let num2 = 15
+let num3 = 38   
+let largest = num1
+
+if (num2 > largest) {
+    largest = num2
+}
+if (num3 > largest) {
+    largest = num3
+}
+console.log(largest)
